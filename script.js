125ba011933fe4ae0e4ef5b57935c9c4aa6c37de
ebalnik <92316771+ebalnik@users.noreply.github.com>
2021-10-11 17:19:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+
+document.addEventListener("keydown", function(event) {
+    jump();
+});
+
+function jump () {
+    if (dino.classList != "jump") {
+        dino.classList.add("jump")
+    }
+    setTimeout (function() {
+        dino.classList.remove("jump")
+    }, 300)
+}
